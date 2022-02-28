c37658746cf2aa924f5085d5062b7409db454aa1
ersincakmak <ckmrsn@gmail.com>
2022-02-28 16:39:20 +0300

    Pagination implementation

--- rick-and-morty/src/components/Characters/index.tsx
+++ rick-and-morty/src/components/Characters/index.tsx
@@ -1,4 +1,6 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { TiArrowLeft, TiArrowRight } from 'react-icons/ti'
+import ReactPaginate from 'react-paginate'
 import { FilterContext } from '../../contexts/FilterContext'
 import { useGetAllCharactersQuery } from '../../generated/graphql'
 import { ICharacter } from '../../types/character'
@@ -8,6 +10,8 @@ import Error from './Error'
 import './style.scss'
 
 function Characters() {
+  const [page, setPage] = useState(0)
+
   const { loading, called, data, refetch } = useGetAllCharactersQuery({
     errorPolicy: 'all',
   })
@@ -17,14 +21,27 @@ function Characters() {
   const { name, gender, species } = useContext(FilterContext)
 
   useEffect(() => {
+    setPage(0)
     refetch({
       filter: {
         gender,
         name,
         species,
       },
+      page: page + 1,
     })
   }, [name, gender, species])
+
+  useEffect(() => {
+    refetch({
+      filter: {
+        gender,
+        name,
+        species,
+      },
+      page: page + 1,
+    })
+  }, [page])
 
   return (
     <div className="content">
@@ -43,6 +60,21 @@ function Characters() {
             />
           ))}
       </div>
+      {data?.characters?.info && (
+        <ReactPaginate
+          pageCount={data.characters.info.pages as number}
+          initialPage={page}
+          forcePage={page}
+          breakLabel="..."
+          className="pagination"
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={3}
+          pageClassName="page"
+          previousLabel={<TiArrowLeft />}
+          nextLabel={<TiArrowRight />}
+          onPageChange={({ selected }) => setPage(selected)}
+        />
+      )}
     </div>
   )
 }
